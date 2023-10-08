7de2729d91843d073869b99a24a2d8b42c2fc4c4
hong-311 <136770134+hong-311@users.noreply.github.com>
2023-10-08 19:28:20 +0900

    라우터 기능을 이용하여 Header컴포넌트 고정
    라우터 기능을 이용하여 Header컴포넌트 고정을 하였습니다

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import NewsPage from './pages/NewsPage';
+import Header from './components/Header';
 
 function App(props) {
   return (
+    <>
+    <Header />
     <Routes>
       <Route path="/:category?" element={<NewsPage />} />
     </Routes>
+    </>
   );
 }
 
