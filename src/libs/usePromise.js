3eaee1c5aea3d6b4fd4e58a33551445eab79a22f
hong-311 <136770134+hong-311@users.noreply.github.com>
2023-10-10 20:59:08 +0900

    주석 추가
    주석을 추가하였습니다

--- src/libs/usePromise.js
+++ src/libs/usePromise.js
@@ -4,21 +4,21 @@ import { useState, useEffect } from 'react';
 
 export default function usePromise(promiseCreator, deps){
     //로딩중, 완료, 실패에 대한 state 관리
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true);
     const [resolved, setResolved] = useState(null);
     const [error, setError] = useState(null);
 
     //컴포넌트 마운트시 데이터 확인
     useEffect(() => {
         const process = async () => {
-            setLoading(true);
+            setLoading(true);   // 로딩중
             try{
-                const resolved = await promiseCreator();
-                setResolved(resolved);
+                const resolved = await promiseCreator();    // 프로미스를 실행하고 완료된 데이터 받아옴
+                setResolved(resolved);  // 완료된 데이터 state에 저장
             } catch(e) {
-                setError(e);
+                setError(e);    // 에러가 발생하면 에러를 state에 저장
             }
-            setLoading(false);
+            setLoading(false);  // 로딩 완료
         };
         process();
     },deps);
