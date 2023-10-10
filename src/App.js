ce650039a1a95e1dbdb561859c58e0f5f23362b1
hong-311 <136770134+hong-311@users.noreply.github.com>
2023-10-10 22:05:11 +0900

    라우터를 활용하여 footer컴포넌트 고정
    라우터 기능을 활용하여 footer 컴포넌트를 고정시켰습니다

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import NewsPage from './pages/NewsPage';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App(props) {
   return (
     <>
     <Header />
     <Routes>
+      <Route path="/" element={<NewsPage />} />
       <Route path="/:category?" element={<NewsPage />} />
     </Routes>
+    <Footer />
     </>
   );
 }
