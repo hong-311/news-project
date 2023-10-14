c9565b5579fb747800c17f2c9b95ae167c575a11
hong-311 <136770134+hong-311@users.noreply.github.com>
2023-10-14 16:24:44 +0900

    Add files via upload

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -38,7 +38,7 @@ function NewsList({category}) {
         const query = category === 'all' ? '' : `&category=${category}`;
 
         return axios.get(
-            `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=0a8c4202385d4ec1bb93b7e277b3c51f`,
+            `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=5c14f62c41a3418d82f80a12321a2729`,
         );
     },[category]);
 
